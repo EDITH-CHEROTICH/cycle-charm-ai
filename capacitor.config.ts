2489c904b95a1fc0363b0d8e02fcc95892bcd32b
EDITH-CHEROTICH <edithcherotich@kabarak.ac.ke>
2025-11-18 16:09:55 +0300

    added android project

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,13 +1,9 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.c5254577076a4067bbf63384a09ec363',
-  appName: 'cycle-charm-ai',
-  webDir: 'dist',
-  server: {
-    url: 'https://c5254577-076a-4067-bbf6-3384a09ec363.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  }
+  appId: 'com.example.app',
+  appName: 'Cycle Charm',
+  webDir: 'dist'
 };
 
 export default config;
